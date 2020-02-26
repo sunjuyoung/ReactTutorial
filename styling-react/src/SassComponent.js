2b2b39ccafb9084e4a673e6de6bf5c7072319776
Sun <syseoz@naver.com>
2020-02-26 23:23:10 +0900

    css styling

---
+++ styling-react/src/SassComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SassComponent.scss'
+
+const SassComponent = () => {
+    return (
+        <div className="SassComponent">
+            <div className="box red"></div>
+            <div className="box orange"></div>
+            <div className="box green"></div>
+        </div>
+    );
+};
+
+export default SassComponent;
