96b557289821e2cb5ba7ce78a2dc7272ca43a647
Sun <syseoz@naver.com>
2020-09-10 22:32:48 +0900

    react todoApp2

---
+++ react-todo-yt/src/App.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react';
+import './App.css';
+
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+  <h1>Ed's Todo List{inputText}</h1>
+    </header>
+    <Form inputText={inputText}setInputText={setInputText} todos={todos} setTodos={setTodos}/>
+    <TodoList todos={todos} setTodos={setTodos}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-todo-yt/src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList =({todos,setTodos})=>{
+  
+    return(
+        <div className="todo-container">
+        <ul className="todo-list">
+       {todos.map(todo=>(
+         <Todo text={todo.text} key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>
+       ))}
+        
+        </ul>
+      </div>
+    );
+};
+
+export default TodoList;
