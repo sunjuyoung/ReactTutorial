268ccfbf6f88aeac84a1aace163fb75b91f19904
Sun <syseoz@naver.com>
2020-11-12 12:31:12 +0900

    movie web

--- movie_app_2020/src/Movie.js
+++ movie_app_2020/src/Movie.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
 const Movie = ({title,summary,poster,year,genres}) => {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title}></img>
         <div className="movie__data">
-            <h3 className="movie__title" style={{backgroundColor:'red'}}>{title}</h3>
+            <h3 className="movie__title">{title}</h3>
             <h3 className="movie__year">{year}</h3>
             <h3 className="movie__summary">{summary}</h3>
             <ul className="movie__genres">
@@ -14,6 +15,7 @@ const Movie = ({title,summary,poster,year,genres}) => {
                     return <li  key={index}className="movie__genre">{genre}</li>
                 })}
             </ul>
+            <p className="movie__summary">{summary.slice(0,100)}......</p>
         </div>
         </div>
     );
